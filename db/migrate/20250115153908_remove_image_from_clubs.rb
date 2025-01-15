d5d9643b2baceb8c47b91075a04577fe0004ad0b
PaulPelerins <paul.pelerins@syndicompare.com>
2025-01-15 16:57:58 +0100

    migration conditionnelle

--- db/migrate/20250115153908_remove_image_from_clubs.rb
+++ db/migrate/20250115153908_remove_image_from_clubs.rb
@@ -1,5 +1,5 @@
 class RemoveImageFromClubs < ActiveRecord::Migration[7.0]
   def change
-    remove_column :clubs, :image, :string
+    remove_column :clubs, :image, :string, if_exists: true
   end
 end

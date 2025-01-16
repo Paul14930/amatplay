e4bb57d4a61db6eea6e34ad5695cc2cd70de769d
PaulPelerins <paul.pelerins@syndicompare.com>
2025-01-16 16:25:50 +0100

    enregistrement a tester

--- app/jobs/handle_scheduled_recording_job.rb
+++ app/jobs/handle_scheduled_recording_job.rb
@@ -2,15 +2,21 @@ class HandleScheduledRecordingJob < ApplicationJob
   queue_as :default
 
   def perform
+    Rails.logger.info "Starting HandleScheduledRecordingJob at #{Time.current}"
+
     now = Time.current
     ScheduledRecording.where(status: "pending", start_time: ..now).find_each do |recording|
+      Rails.logger.info "Starting recording for camera #{recording.camera.id}"
       CloudflareService.new.start_recording(recording.camera.live_input_id)
       recording.update(status: "active")
     end
 
     ScheduledRecording.where(status: "active", end_time: ..now).find_each do |recording|
+      Rails.logger.info "Stopping recording for camera #{recording.camera.id}"
       CloudflareService.new.stop_recording(recording.camera.live_input_id)
       recording.update(status: "completed")
     end
+
+    Rails.logger.info "Completed HandleScheduledRecordingJob at #{Time.current}"
   end
 end
